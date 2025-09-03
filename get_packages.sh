e8077d0190d025bd9aa267bc6cd4462494e23d8f
gailoks <yra.cimonov@gmail.com>
2025-09-03 14:06:53 +0300

    added get_packages.sh

---
+++ get_packages.sh
@@ -0,0 +1,6 @@
+mkdir packages
+wget --progress=dot:giga \
+        -O packages/gosu \
+        "https://github.com/tianon/gosu/releases/download/1.17/gosu-amd64"
+wget -O packages/MangoHud.tar.gz https://github.com/flightlessmango/MangoHud/releases/download/v0.8.1/MangoHud-0.8.1.r0.gfea4292.tar.gz
+wget -O packages/heroic.deb https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.18.1/Heroic-2.18.1-linux-amd64.deb
