87f62fba4be06f502bd56573c6ab65ac2052ad49
Gailoks <yra.cimonov@gmail.com>
2025-11-23 12:48:00 +0000

    Updated start * Fixing scaling issue * Default scale is now 100%

--- session-init.sh
+++ session-init.sh
@@ -33,6 +33,8 @@ fi
 
 echo ">> Applying GNOME scaling settings"
 gsettings set org.gnome.desktop.interface scaling-factor 1
+gsettings set org.gnome.desktop.interface text-scaling-factor 1
+
 
 echo ">> Configuring sway + Xwayland"
 export DISPLAY=:10
--- start-de.sh
+++ start-de.sh
@@ -7,7 +7,7 @@ SLEEP_TIME="${SLEEP_TIME:-1}"
 echo ">> start-de.sh: Waiting for {} mode..."
 
 for ((i=1; i<=MAX_RETRIES; i++)); do
-    if xrandr --output XWAYLAND0 --mode "${GAMESCOPE_WIDTH}x${GAMESCOPE_HEIGHT}" >/dev/null 2>&1; then
+    if xrandr --output XWAYLAND0 --mode "${GAMESCOPE_WIDTH}x${GAMESCOPE_HEIGHT}" &> /dev/null; then
         echo ">> start-de.sh: Mode set successfully"
         break
     fi
